dd9b55f655363ee39b7614f31069b7c94079eb1a
alex <aobustillos@mavs.coloradomesa.edu>
2024-03-06 12:47:27 -0700

    sync

--- HW1/database.cpp
+++ HW1/database.cpp
@@ -69,6 +69,7 @@ void databases::database::setdb_id(string newdb_id)
 //     }
 //     //parse line
 // }
+//I am just setting every movie in the array to a new movie/giving it memory to work off of
 void databases::database::initmovies()
 {
     for (int i = 0; i < 99; i++)
@@ -77,6 +78,7 @@ void databases::database::initmovies()
         movieList[i] = newmovie;
     }
 }
+// Once the user exits the loop this fucntion is called that deletes every movie list and frees the memory that the code initially created
 void databases::database::freemem()
 {
     for (int i = 0; i < 99; i++)
@@ -84,25 +86,40 @@ void databases::database::freemem()
         delete movieList[i];
     }
 }
+//Reading the file from a csv file
 void databases::database::readFile()
 {
+    // I set the filename equal to nothing, it doesnt matter because its going to be replaced anyway
     string filename = "";
+    //ifstrema is going to be called with input
     ifstream input;
+    //I get the filename
     cout << "What is the filename?"<< endl;
     cin >> filename;
+    //I then open the filename with ifstream
     input.open(filename);
+    //a new string that is equl to nothing
     string line = "";
-    
-    int rowCounter = 0;
+    //This counts the rows in the csv, meaning the i variable will technically be column counter, it is set to arrSize because arrSize is 0 and this is how
+    //it knows how much data we are currently holding in our movielist
+    int rowCounter = arrSize;
+    // While we are reading the file and putting it into line
     while (getline(input, line))
     {
-        
+        // column counter
         int i = 0;
+        // string stream is neccessary in order to parse the csv file and extract the data properly
         stringstream inputString;
+        // a tmp variable to be able to put the data into the movielist
         string tmp;
+        // this returns a copy of the contents of the stream
         inputString.str(line);
+        // While we are reading the file from a csv file parsing it, there is two because its a double for loop that works much better
         while (getline(inputString, tmp, ','))
         {
+            // the rowcounter is set to 1, meaning we are reading the first line of data, now we are putting the data into movielist with a loop that goes throough evert 
+            // part in that first line untill the line is done then it changes the row counter to go to the next line and restarts this column counter till every part of the
+            //csv file is put into the movieList
             if (i == 0)
             {
                 movieList[rowCounter]->setId(tmp);
@@ -130,17 +147,22 @@ void databases::database::readFile()
             i++;
             
         }
-
+        // at the end rowcounte is incremented which is then set equal to arrSize
         rowCounter++;
         arrSize = rowCounter;
     }
+    //closing the file after use
     input.close();
 }
+//Print the current movies and movie data held in movieList
 void databases::database::printMovies()
 {
+    //this is a menu to help read the print
     cout <<"ID"<<setw(13)<<"Title"<<setw(60)<<"Year"<< setw(7)<< "Genre"<< setw(19)<< "Director"<< endl;
+    //a loop that loops arrsize, arrsize is how large our database of movies is
     for(int i = 0; i < arrSize; i++)
     {
+        //getting the data using the getters i made
         cout << left << setw(9) << movieList[i]->getId()<<" "
         <<setw(60) << movieList[i]->getTitle()<<" " 
         <<setw(5) << movieList[i]->getYear()<<" "
@@ -151,9 +173,10 @@ void databases::database::printMovies()
 
 }
 
+//adding individual movies to the list rather then readign from a file
 void databases::database::addMovie()
 {
-    
+    // asking the user to properly enter the info then making a tmp variable to add to the movieList properly
     string tmp;
     cout << "Enter movie data in order of, ID TITLE YEAR GENRE DIRECTOR: "<< endl;
     cin >> tmp;
@@ -168,18 +191,24 @@ void databases::database::addMovie()
     movieList[arrSize]->setDirector(tmp);
     arrSize++;
 }
+//removing an individual movie 
 void databases::database::removeMovie()
 {
     // arrSize = arrSize-1;
     string movieID = "";
+    //asking for what movie they want to delete by getting the ID
     cout << "What movie do you want to delete?(ID): " <<endl;
     cin >> movieID;
+    //I am searching through every single movie list till i find the id
     for(int i = 0; i <= arrSize; i++)
     {
         if(movieList[i]->getId() == movieID)
         {
+            //I am going through the list starting at the point where we found where the id from the user is equal to the id from the movie list
+            // i am going through arrsize-1 because i am subtracting a movie
             for (int j = i; j <= arrSize-1; j++)
             {
+                //I am moving all the movies up by one instead of directly deleting the movie 
                 movieList[j] = movieList[j+1];
 
                 // movieList[j]->getId() = movieList[j+1]->getId();
@@ -189,24 +218,31 @@ void databases::database::removeMovie()
                 // movieList[j]->getDirector() = movieList[j+1]->getDirector();
 
             }
+            //arrSize is now one less
         arrSize = arrSize - 1;
         // cout <<"DEBUG: "<< arrSize << endl;
         }
     }
 
 }
+//searching for a movie in the database
 void databases::database::searchMovies()
 {   
+    // a little redundent but the assignment did say search the database with a tittle or genre
     int answer;
     string movieTitle = "";
     string movieGenre = "";
     cout << "Search movie by Tile[1] or Genre[2]? "<<endl;
     cin >> answer;
+    //once the option is chosen
     if (answer == 1)
     {
+        //ask for the title
         cout << "What is the Title?"<< endl;
         cin >> movieTitle;
+        //menu
         cout <<"ID"<<setw(13)<<"Title"<<setw(60)<<"Year"<< setw(7)<< "Genre"<< setw(19)<< "Director"<< endl;
+        //going through the movielist looking for the movie that matches the name 
         for(int i = 0; i < arrSize; i++)
         {
             if(movieList[i]->getTitle()== movieTitle)
@@ -219,6 +255,7 @@ void databases::database::searchMovies()
             }
         }
     }
+    //this is the same thing but looping for the movies that match the genre
     if (answer == 2)
     {
         cout << "What is the Genre(capital first letter)?"<< endl;
@@ -238,12 +275,15 @@ void databases::database::searchMovies()
     }
 
 }
+//this makes a csv file and prints the current movielist into the new file
 void databases::database::transferData()
 {
+    //ofstream uses my file then creates a csv file
     ofstream myfile("newMovies.csv");
-
+    //goes through the whole arrSize
     for(int i = 0 ; i < arrSize; i++)
     {
+        //printing into the file
         myfile << movieList[i]->getId()<<",";
         myfile << movieList[i]->getTitle()<<",";
         myfile << movieList[i]->getYear()<<",";
@@ -251,5 +291,6 @@ void databases::database::transferData()
         myfile << movieList[i]->getDirector()<<endl;
 
     }
+    //closing when done
     myfile.close();
 }
--- HW1/main.cpp
+++ HW1/main.cpp
@@ -8,12 +8,15 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    //in order to make a menu i will make a options setting so the user can choose what they want to do
     int options = 0;
+    //I am creating a database to be able to use all the functions that have access to variables from database
     databases::database Mydatabase;
+    //While the user doesnt want to exit, if the user pressed 7 then the while loop stops
     while(options !=7)
     {
+        //Menu
         cout<<"-------------------------"<< endl;
-
         cout<<"[1]Add Movies"<<endl;
         cout<< "[2]Delete Movies"<< endl;
         cout<< "[3]Print Current Movies"<<endl;
@@ -22,9 +25,9 @@ int main(int argc, char* argv[])
         cout<< "[6]Read a file"<< endl;
         cout<< "[7]Stop running"<<endl;
         cout<<"-------------------------"<< endl;
-
+        //asking for the option
         cin >> options;
-
+        // whatever the user chooses it calls the functions then restarts the loop
         if(options == 1)
         {  
             Mydatabase.addMovie();
@@ -51,9 +54,8 @@ int main(int argc, char* argv[])
         {
             Mydatabase.readFile();
         }
-
-
     }
+    //once the user wants to exit i call a fucntion to free the memory
     if (options == 7)
     {
         Mydatabase.freemem();
